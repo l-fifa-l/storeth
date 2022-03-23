bca60208de3d59563930123f7aa87381d10be63d
Vivek <starktestic@gmail.com>
2022-03-23 14:08:49 +0530

    base layout 1

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import { Hero, Breadcrumbs } from '@components/common';
+import { CourseList } from '@components/course';
+import { BaseLayout } from '@components/layout';
+import { OrderCard } from '@components/order';
+import { EthRates, WalletBar } from '@components/web3';
+
+export default function Home() {
+  return (
+    <BaseLayout>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </BaseLayout>
+  );
+}
