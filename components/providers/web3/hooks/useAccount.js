b5ac04bb25ac3def5beaae2a87fcc917ce9c5e2c
Vivek <starktestic@gmail.com>
2022-03-28 15:48:38 +0530

    hooks finished

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,5 +1,30 @@
-export const handler = (web3) => () => {
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
+const adminAddresses = {
+  '0x22e4fae15324a9412c7c6e00611bd061196c8dd42f2a4f222c5952e7269ea74a': true,
+};
+
+export const handler = (web3, provider) => () => {
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? 'web3/accounts' : null),
+    async () => {
+      const accounts = await web3.eth.getAccounts();
+      return accounts[0];
+    }
+  );
+
+  useEffect(() => {
+    provider &&
+      provider.on('accountsChanged', (accounts) => mutate(accounts[0] ?? null));
+  }, [provider]);
+
   return {
-    account: web3 ? 'Test Account' : 'null',
+    account: {
+      data,
+      isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
+      mutate,
+      ...rest,
+    },
   };
 };
