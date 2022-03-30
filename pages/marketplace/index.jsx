24d6b494baea55fe1681bbfb99d055a348a501e0
Vivek <starktestic@gmail.com>
2022-03-30 16:09:04 +0530

    Oder

--- pages/marketplace/index.jsx
+++ pages/marketplace/index.jsx
@@ -3,8 +3,12 @@ import { BaseLayout } from '@components/ui/layout';
 import { getAllCourses } from '@content/courses/fetcher';
 import { WalletBar } from '@components/ui/web3';
 import { useAccount, useNetwork } from '@components/hooks/web3';
+import { Button } from '@components/ui/common';
+import { OrderModal } from '@components/ui/order';
+import { useState } from 'react';
 
 export default function Marketplace({ courses }) {
+  const [selectedCourse, setSelectedCourse] = useState(null);
   const { account } = useAccount();
   const { network } = useNetwork();
   return (
@@ -21,8 +25,29 @@ export default function Marketplace({ courses }) {
         />
       </div>
       <CourseList courses={courses}>
-        {(course) => <CourseCard key={course.id} course={course} />}
+        {(course) => (
+          <CourseCard
+            key={course.id}
+            course={course}
+            Footer={() => (
+              <div className="mt-4">
+                <Button
+                  onClick={() => setSelectedCourse(course)}
+                  variant="lightPurple"
+                >
+                  Purchase
+                </Button>
+              </div>
+            )}
+          />
+        )}
       </CourseList>
+      {selectedCourse && (
+        <OrderModal
+          course={selectedCourse}
+          onClose={() => setSelectedCourse(null)}
+        />
+      )}
     </>
   );
 }
